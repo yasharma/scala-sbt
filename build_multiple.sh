30969e2fc5d450b63f682d139ba245ca3aa0635f
Arturas Smorgun <arturas@spikerlabs.com>
2017-12-11 22:35:10 +0000

    Update tagged versions

--- build_multiple.sh
+++ build_multiple.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 scala_versions=(
-  2.11.11
-  2.12.3
+  2.10.7
+  2.11.12
+  2.12.4
 )
 sbt_versions=(
-  0.13.13
-  0.13.15
   0.13.16
+  1.0.4
 )
 for scala_version in ${scala_versions[@]}; do
   for sbt_version in ${sbt_versions[@]}; do
